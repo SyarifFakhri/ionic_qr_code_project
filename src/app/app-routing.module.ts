e4068961aa2d79926eded07b882a34a5cafde0a8
aqeelaimi <aqeel.6468@gmail.com>
2019-03-14 02:08:40 +0800

    auth login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/services/user/auth.guard';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule',canActivate: [AuthGuard], },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
   { path: 'qrcode', loadChildren: './qrcode/qrcode.module#QrcodePageModule' },
@@ -13,12 +17,18 @@ const routes: Routes = [
   { path: 'code', loadChildren: './code/code.module#CodePageModule' },
   { path: 'menu-class', loadChildren: './menu-class/menu-class.module#MenuClassPageModule' },
   { path: 'attendance', loadChildren: './attendance/attendance.module#AttendancePageModule' },
+  {path: 'event-create',loadChildren:'./pages/event-create/event-create.module#EventCreatePageModule',canActivate: [AuthGuard],},
+  { path: 'event-detail/:id',loadChildren:'./pages/event-detail/event-detail.module#EventDetailPageModule', canActivate: [AuthGuard],},
+  { path: 'reset-password', loadChildren: './reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+
+
  // { path: 'attendance', loadChildren: './attendance/attendance.module#AttendancePageModule' },
  // { path: 'alert', loadChildren: './alert/alert.module#AlertPageModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),ReactiveFormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/credentials.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    
+        apiKey: "AIzaSyALQWN9mCj45xLQ0oGBawFeiyM4c5Tbq44",
+        authDomain: "qrcode-d469a.firebaseapp.com",
+        databaseURL: "https://qrcode-d469a.firebaseio.com",
+        projectId: "qrcode-d469a",
+        storageBucket: "qrcode-d469a.appspot.com",
+        messagingSenderId: "706322031641"
+    
+    
+  };
