3e7485760b8d18bd3d37a50c12436fe2a696b0ec
ameliacupcake <ameliashafina3995@gmail.com>
2019-03-20 16:00:10 +0800

    edit reset button

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
     
-        apiKey: "AIzaSyALQWN9mCj45xLQ0oGBawFeiyM4c5Tbq44",
-        authDomain: "qrcode-d469a.firebaseapp.com",
-        databaseURL: "https://qrcode-d469a.firebaseio.com",
-        projectId: "qrcode-d469a",
-        storageBucket: "qrcode-d469a.appspot.com",
-        messagingSenderId: "706322031641"
+      apiKey: "AIzaSyBIsOj07X52nuCT5WdvrpHN3HPU6jtoEQo",
+      authDomain: "login-bdfa1.firebaseapp.com",
+      databaseURL: "https://login-bdfa1.firebaseio.com",
+      projectId: "login-bdfa1",
+      storageBucket: "login-bdfa1.appspot.com",
+      messagingSenderId: "167169397872"
     
     
   };
