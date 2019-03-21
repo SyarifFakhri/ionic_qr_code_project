821f0b13ddf8c3340377df303f6bf7c9788a2b0f
syariffakhri <asybaf@gmail.com>
2019-03-21 10:11:38 +0800

    fixed slow loading on database

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -6,6 +6,10 @@ export const firebaseConfig = {
       projectId: "login-bdfa1",
       storageBucket: "login-bdfa1.appspot.com",
       messagingSenderId: "167169397872"
-    
-    
+    // apiKey: "AIzaSyCWqUawYopdLp8IT0CPnFfeGycIKLoDc-4",
+    // authDomain: "attendanceapp-7eee4.firebaseapp.com",
+    // databaseURL: "https://attendanceapp-7eee4.firebaseio.com",
+    // projectId: "attendanceapp-7eee4",
+    // storageBucket: "",
+    // messagingSenderId: "659569133431"
   };
--- src/app/services/class-info.service.ts
+++ src/app/services/class-info.service.ts
@@ -19,43 +19,33 @@ export class ClassInfoService {
   private classCollection: AngularFirestoreCollection<any>;
   private userID: string = "lecturer1";
 
-  private classList: Observable<ClassListInterface[]>;
+  public classList: Observable<ClassListInterface[]>;
   
   constructor(db: AngularFirestore) {
-
     this.classCollection = db.collection<any>('users');
-    
-    //this.classList = this.classCollection.snapshotChanges().pipe(
-    this.classList = this.classCollection.doc(this.userID).collection<any>("class").snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data();
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        });
-      })
-    );
-  }
- 
-  getDetails() {
-    return this.classList;
+    this.classList = this.classCollection.doc(this.userID).collection<ClassListInterface>("class").valueChanges();
+
   }
 
   getDetail(id) {
     //return this.classCollection.doc<ClassListInterface>(id).valueChanges();
+    console.log("get details of: " + id);
     return this.classCollection.doc(this.userID).collection<any>("class").doc<ClassListInterface>(id).valueChanges();
   }
  
   updateDetail(classDetails: ClassListInterface) {
+    console.log("update detail");
     return this.classCollection.doc(this.userID).collection<any>("class").doc(classDetails.id).update(classDetails);
   }
  
   addDetail(classDetails: ClassListInterface) {
+    console.log("Add detail");
     this.classCollection.doc(this.userID).set({"username":this.userID});
     return this.classCollection.doc(this.userID).collection<any>("class").doc(classDetails.id).set(classDetails);
   }
  
   removeDetail(id) {
+    console.log("remove detail")
     return this.classCollection.doc(id).delete();
   }
 }
